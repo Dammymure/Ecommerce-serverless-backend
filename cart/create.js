de599faa12fb9c4f742f4de6893bc17eaa72e155
Dammymure <109584109+Dammymure@users.noreply.github.com>
2023-09-06 00:48:59 +0100

    Complete E-commerce serverless backend using Aircode

---
+++ cart/create.js
@@ -0,0 +1,37 @@
+// @see https://docs.aircode.io/guide/functions/
+const aircode = require('aircode');
+const {verifyToken} = require('../helper/verifyToken')
+
+
+module.exports = async function (params, context) {
+  const tokenUser = await verifyToken(context)
+  if(tokenUser != null){
+    const {products} =params;
+    if(!products){
+      context.status(400)
+      return{
+        "message": "Products are mandatory"
+      }
+    }
+    const cartTable = aircode.db.table("cart")
+
+    try{
+      const cart = {
+        ...params,
+        userId: tokenUser._id
+      }
+      const result = await cartTable.save(cart)
+      context.status(201)
+      return{
+        result  
+      }
+      
+    }catch(err){
+      context.status(500)
+      return{"message": err.message}
+    }
+  }else{
+    context.status(401)
+    return{"message":"Token in invalid or user is not authorized"}
+  }
+};
